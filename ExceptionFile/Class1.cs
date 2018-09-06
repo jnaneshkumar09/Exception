0063807ed90dbeb5ea066ea39c11807dabf95963
jnanesh kumar <jnanesh@digixhub.com>
2018-09-06 12:54:29 +0530

    initial

---
+++ ExceptionFile/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionFile
+{
+    public static class ErrorLog
+    {
+        static string constr = ConfigurationManager.ConnectionStrings["constr1"].ConnectionString;
+        public static void SendExceptionToDB(Exception exdb)
+        {
+            //string exepurl = System.Web.HttpContext.Current.Request.Url.ToString();            
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand com = new SqlCommand("ExceptionLoggingToDataBase", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@ExceptionMsg", exdb.Message.ToString());
+            com.Parameters.AddWithValue("@ExceptionType", exdb.GetType().Name.ToString());
+            com.Parameters.AddWithValue("@ExceptionURL", "http://localhost:59449/WebForm1.aspx");
+            com.Parameters.AddWithValue("@ExceptionSource", exdb.StackTrace.ToString());
+            com.ExecuteNonQuery();
+        }       
+
+    }       
+
+    
+}
